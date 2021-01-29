48c3cfdc5489abc3f9976833283ab8f22d3ce508
zerrin-gif <72238583+zerrin-gif@users.noreply.github.com>
2021-01-29 15:41:10 -0600

    Add files via upload

---
+++ About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class About extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is my About page!</p>
+      </div>
+    )
+  }
+}
+export default About;
---
+++ Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+ class Home extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is my Home page!</p>
+      </div>
+    )
+  }
+}
+export default Home;
